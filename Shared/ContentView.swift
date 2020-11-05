80a790f78a4b8c3e9b9c4699712980492b21e10b
Thomas Step <tstep916@gmail.com>
2020-11-05 17:54:06 -0600

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Thomas Step on 11/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Pomodoro_NoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_NoiseApp.swift
+//  Shared
+//
+//  Created by Thomas Step on 11/5/20.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_NoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
